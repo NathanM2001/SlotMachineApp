fa60086b20aa48dd259ca3f3d04a2e4796017796
nathanmckenzie <ngm18@acu.edu>
2020-11-23 14:59:58 -0600

    Initial Commit

---
+++ SlotMachineApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlotMachineApp
+//
+//  Created by Nathan Mckenzie on 11/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
